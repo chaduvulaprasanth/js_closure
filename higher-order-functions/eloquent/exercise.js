0bd4e1bf78554631960aeec25a8ecd93ce52dc62
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-08 23:46:09 +0530

    updated eloquent js exercise

--- higher-order-functions/eloquent/exercise.js
+++ higher-order-functions/eloquent/exercise.js
@@ -2,10 +2,17 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // Your code here.
+arrays.flat()
 // → [1, 2, 3, 4, 5, 6]
 
 // Challenge 2. Your own loop
 // Your code here.
+
+function loop(start, test, update, body) {
+  for (let n = start; test(n); n = update(n)) {
+    body(n);
+  }
+}
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
@@ -13,9 +20,21 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 1
 
 // Challenge 3. Everything
+// Your code here.
+// one version using method
 function every(array, test) {
-  // Your code here.
+  return array.every(test)
 }
+
+// second version using loop
+function every(array, test) {
+  for (let key in array) {
+    if(test(array[key])) {
+      return true;
+    }
+
+    }
+  }
 
 console.log(every([1, 3, 5], n => n < 10));
 // → true
