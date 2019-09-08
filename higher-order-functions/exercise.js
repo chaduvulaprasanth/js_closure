dca67965c2cc5e6336b74c28ad61e9820c3ca9a1
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-08 17:35:25 +0530

    updatedd higher-order functions exercise

--- higher-order-functions/exercise.js
+++ higher-order-functions/exercise.js
@@ -1,23 +1,31 @@
 // Challenge 1
 function addTwo(num) {
-    return (n+2);
+    return (num+2);
 }
 
 // To check if you've completed it, uncomment these console.logs!
-// console.log(addTwo(3));
-// console.log(addTwo(10));
+console.log(addTwo(3));
+console.log(addTwo(10));
 
 // Challenge 2
-function addS(word) {}
+function addS(word) {
+    return word + "s";
+}
 
 // uncomment these to check your work
-// console.log(addS('pizza'));
-// console.log(addS('bagel'));
+console.log(addS('pizza'));
+console.log(addS('bagel'));
 
 // Challenge 3
-function map(array, callback) {}
+function addTwo(num) {
+    return (num + 2);
+}
 
-// console.log(map([1, 2, 3], addTwo));
+function map(array, callback) {
+    return array.map(callback);
+}
+
+console.log(map([1, 2, 3], addTwo));
 
 // Challenge 4
 function forEach(array, callback) {}
